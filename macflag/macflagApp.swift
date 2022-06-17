0df834d8a25602f5215d6ba476c82be3ed3d38ff
Eric May <erictmay@me.com>
2022-06-17 11:56:57 -0400

    Initial Commit

---
+++ macflag/macflagApp.swift
@@ -0,0 +1,17 @@
+//
+//  macflagApp.swift
+//  macflag
+//
+//  Created by Eric May on 6/17/22.
+//
+
+import SwiftUI
+
+@main
+struct macflagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
