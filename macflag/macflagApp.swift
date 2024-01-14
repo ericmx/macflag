26dcc0fbb9ff33be6cdd7e0958c85521e9f4a094
Eric May <eric.may@boldpenguin.com>
2024-01-14 14:42:40 -0500

    Refactored to remove the toggle for the dock icon, and added a quit button to the top of the menu.

--- macflag/macflagApp.swift
+++ macflag/macflagApp.swift
@@ -9,30 +9,26 @@ import SwiftUI
 
 @main
 struct MacFlag: App {
-        
     var menuBarFlag = Flag().flags
     @State var currentFlag = "🏴‍☠️"
-    @State var isDockdisabled = false
     
     var body: some Scene {
+        
         MenuBarExtra(currentFlag) {
-            Button(action: {
-                toggleDockIconEnabled(isDockdisabled)
-            }) {
-                Text("Toggle dock icon")
-            }
-            ForEach(menuBarFlag.indices, id: \.self) { index in
-                Button(action: {
-                    currentFlag = menuBarFlag[index]
-                }) {
-                    Text("\(menuBarFlag[index])").font(.title)
-                }
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }.keyboardShortcut("q")
+            
+            Divider()
+            
+                ForEach(menuBarFlag.indices, id: \.self) { index in
+                    Button(action: {
+                        currentFlag = menuBarFlag[index]
+                    }) {
+                        Text("\(menuBarFlag[index])").font(.title)
+                    }
             }
         }
     }
-    
-    func toggleDockIconEnabled(_ isEnabled: Bool) {
-        NSApp.setActivationPolicy(isEnabled ? .regular : .accessory)
-    }
-
 }
+
